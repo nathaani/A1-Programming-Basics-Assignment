ecb2bbb9d9cca6706d3260c7f60ee9dffc306919
nathaani <144153847+nathaani@users.noreply.github.com>
2023-09-13 12:54:33 -0400

    A1 Part 1:
    Create a data structure that describes a "hospital". It has a name and contains 3 "patient" records.
    
    Each "patient" has an id, fullName, dateOfBirth, and multiple "symptoms".
    
    Each symptom in a String.

---
+++ A1Hospital.js
@@ -0,0 +1,37 @@
+
+let hospitalName = "Toronto Hospital"
+let patients = [
+    {
+        id: "001",
+        fullName: "Natalie Lew",
+        dateOfBirth: new Date (1995,3,10),
+        symptoms: 
+            [
+                {cough: "cough"},
+                {sneeze: "sneeze"},
+                {rash: "rash"}
+            ]
+    },
+    {
+        id: "002",
+        fullName: "Ben Ten",
+        dateOfBirth: new Date (1992,12,3),
+        symptoms: 
+            [
+                {fever: "fever"},
+                {swelling: "swelling"},
+                {nausea: "nausea"}
+            ]
+    },
+    {
+        id: "003",
+        fullName:"Chris Mas",
+        dateOfBirth: new Date (1970,12,31),
+        symptoms:
+            [
+                {highTemperature: "high temperature"},
+                {cough: "cough"},
+                {increaseApetite: "increase in apetite"}
+            ]
+    }
+]
